cfcf87b354ea6aab40bf1c620ee882f5f4a702b8
ilia makarov <ilia.makarov@me.com>
2022-06-14 16:51:22 +0300

    fix error message

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,5 +1,7 @@
 import { FC, useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
 import {
+    P5,
     H3,
     useDeskproAppClient,
     useDeskproOAuth2Auth,
@@ -8,48 +10,61 @@ import { useStore } from "../context/StoreProvider/hooks";
 import { checkIsAliveService } from "../services/trello";
 import { Loading, AnchorButton } from "../components/common";
 
+const LogInError = styled(P5)`
+    margin-bottom: 8px;
+    color: ${({ theme }) => theme.colors.scarlett100};
+`;
+
 const LogIn: FC = () => {
     const { client } = useDeskproAppClient();
     const { callback } = useDeskproOAuth2Auth("token", /#token=(?<token>[0-9a-f]+)$/);
     const [state, dispatch] = useStore();
     const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
     const authUrl = `https://trello.com/1/authorize?expiration=1day&name=MyPersonalToken&scope=read&response_type=token&key=${state?.context?.settings.client_key}&redirect_uri=${callback?.callbackUrl}`
 
-    const onSignIn = useCallback(() => {
-        if (!client) {
-            return;
-        }
+    if (error) {
+        console.error(`Trello: ${error}`);
+    }
 
+    const onSignIn = useCallback(() => {
         callback?.poll()
             .then(() => dispatch({ type: "setAuth", isAuth: true }))
             .catch((error) => dispatch({ type: "error", error }));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [client, callback]);
+    }, [callback]);
 
     useEffect(() => {
-        if (!client) {
+        if (!client || !callback?.hasToken) {
             return;
         }
 
-        setLoading(true)
+        setLoading(true);
+        setError(null);
 
-        checkIsAliveService(client)
+        callback?.hasToken()
+            .then((hasToken) => {
+                if (hasToken) {
+                    return checkIsAliveService(client);
+                }
+            })
             .then((data) => {
                 if (data?.isAlive) {
                     dispatch({ type: "setAuth", isAuth: true });
                     dispatch({ type: "changePage", page: "home" })
                 }
             })
-            .catch((error) => dispatch({ type: "error", error }))
+            .catch((error) => setError(error))
             .finally(() => setLoading(false));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [client]);
+    }, [client, callback?.hasToken]);
 
     return loading
         ? (<Loading/>)
         : (
             <>
-                <H3 style={{ marginBottom: 14 }}>Log into your Trello Account</H3>
+                <H3 style={{ marginBottom: !error ? 14 : 2 }}>Log into your Trello Account</H3>
+                {error && (<LogInError>An error occurred, please try again.</LogInError>)}
                 <AnchorButton text="Sign In" href={authUrl} target="_blank" onClick={onSignIn}/>
             </>
         );
