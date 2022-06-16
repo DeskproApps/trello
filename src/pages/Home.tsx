f5785652b31559eb1e3d19fd8d3485be8ee8fe44
ilia makarov <ilia.makarov@me.com>
2022-06-16 19:15:18 +0300

    List linked Trello cards on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,31 @@
-import { FC, useEffect, useState } from "react";
-import { useDeskproAppClient } from "@deskpro/app-sdk";
+import { FC, useEffect, useState, Fragment, ChangeEvent } from "react";
+import {
+    HorizontalDivider,
+    useDeskproAppClient,
+} from "@deskpro/app-sdk";
 import { useStore } from "../context/StoreProvider/hooks";
 import { useSetAppTitle } from "../hooks";
-import { checkIsLinkedCardsService } from "../services/entityAssociation";
-import { Loading } from "../components/common";
+import { getEntityCardListService } from "../services/entityAssociation";
+import { getCardService } from "../services/trello";
+import { Loading, CardInfo, NoFound, InputSearch } from "../components/common";
+
+const getFilteredCards = (cards: any[], searchValue: string) => {
+    let filteredCards = [];
+    if (!searchValue) {
+        filteredCards = cards;
+    } else {
+        filteredCards = cards.filter(({ name }) => name.toLowerCase().includes(searchValue.toLowerCase()))
+    }
+
+    return filteredCards;
+};
 
 const Home: FC = () => {
     const { client } = useDeskproAppClient();
     const [state, dispatch] = useStore();
     const [loading, setLoading] = useState<boolean>(false);
+    const [cards, setCards] = useState<any[]>([]);
+    const [searchCard, setSearchCard] = useState<string>("");
     const ticketId = state.context?.data.ticket.id
 
     useSetAppTitle("Trello Cards");
@@ -33,12 +50,21 @@ const Home: FC = () => {
 
         setLoading(true);
 
-        checkIsLinkedCardsService(client, ticketId)
-            .then((isLinked) => {
-                if (!isLinked) {
-                    dispatch({ type: "changePage", page: "link_card" });
+        getEntityCardListService(client, ticketId)
+            .then((cardIds) => {
+                if (Array.isArray(cardIds)) {
+                    if (cardIds.length > 0) {
+                        return Promise.all(cardIds.map((cardId) => {
+                            return getCardService(client, cardId);
+                        }))
+                    } else {
+                        dispatch({ type: "changePage", page: "link_card" });
+                    }
+                } else {
+                    return Promise.reject(false);
                 }
             })
+            .then((cards) => setCards(cards))
             .catch((error) => {
                 dispatch({ type: "error", error })
             })
@@ -50,9 +76,23 @@ const Home: FC = () => {
         ? (<Loading/>)
         : (
             <>
-                HomePage
+                <InputSearch
+                    value={searchCard}
+                    onClear={() => setSearchCard("")}
+                    onChange={(e) => setSearchCard(e.target.value)}
+                />
+                <HorizontalDivider style={{ marginBottom: 9 }}/>
+                {cards.length === 0
+                    ? (<NoFound/>)
+                    : getFilteredCards(cards, searchCard).map(({ id, ...card }) => (
+                        <Fragment key={id}>
+                            <CardInfo {...card} />
+                            <HorizontalDivider style={{ marginBottom: 9 }}/>
+                        </Fragment>
+                    ))
+                }
             </>
-        );
+        )
 };
 
 export { Home };
