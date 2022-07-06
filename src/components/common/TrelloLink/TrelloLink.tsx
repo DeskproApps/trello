60a4621a39378daf8b5e6e3883396e7f177a61eb
ilia makarov <ilia.makarov@me.com>
2022-07-06 10:50:25 +0300

    BrandLink component now is whole pill be clickable

--- src/components/common/TrelloLink/TrelloLink.tsx
+++ src/components/common/TrelloLink/TrelloLink.tsx
@@ -15,6 +15,11 @@ const Container = styled(Stack)`
 `;
 
 const Link = styled.a`
+    :hover {
+      border-color: ${({ theme }) => (theme.colors.brandShade60)};
+    };
+    border-radius: 9px;
+    border: 1px solid transparent;
     color: ${({ theme }) => (theme.colors.brandShade100)}
 `;
 
@@ -29,22 +34,24 @@ const TrelloLink = ({ href }: Props) => {
     const { theme } = useDeskproAppTheme();
 
     return (
-        <RoundedLabelTag
-            size="small"
-            withClose={false}
-            backgroundColor={theme.colors.brandShade20}
-            textColor="#4C4F50"
-            borderColor={theme.colors.brandShade20}
-            closeIcon={faArrowUpRightFromSquare}
-            label={(
-                <Container>
-                    <TrelloIcon icon={<TrelloLogo/>} />
-                    <Link target="_blank" href={href}>
-                        <Icon icon={faArrowUpRightFromSquare} />
-                    </Link>
-                </Container>
-            )}
-        />
+        <Link target="_blank" href={href}>
+            <RoundedLabelTag
+                size="small"
+                withClose={false}
+                backgroundColor={theme.colors.brandShade20}
+                textColor={theme.colors.grey100}
+                borderColor={theme.colors.brandShade20}
+                closeIcon={faArrowUpRightFromSquare}
+                label={(
+                    <Container>
+                        <TrelloIcon icon={<TrelloLogo/>} />
+                        <Stack>
+                            <Icon icon={faArrowUpRightFromSquare} />
+                        </Stack>
+                    </Container>
+                )}
+            />
+        </Link>
     );
 }
 
