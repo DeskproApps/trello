f0981d5adf5b077c26be0bbc7d313c76f26e6312
Ashley Dawson <ashley.dawson@deskpro.com>
2023-05-11 15:50:14 +0100

    Replaced auth process

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,119 +1,73 @@
-import { FC, useEffect, useState } from "react";
-import styled from "styled-components";
 import {
-  P5,
   H3,
+  OAuth2CallbackUrl,
   useDeskproAppClient,
-  OAuth2StaticCallbackUrl,
-  IDeskproClient,
+  useDeskproLatestAppContext,
+  useInitialisedDeskproAppClient
 } from "@deskpro/app-sdk";
-import { useStore } from "../context/StoreProvider/hooks";
-import { useSetAppTitle } from "../hooks";
-import { getQueryParams } from "../utils";
-import { Loading, AnchorButton } from "../components/common";
-import { checkIsAliveService } from "../services/trello";
+import { AnchorButton } from "../components/common";
+import { useEffect, useState } from "react";
 
-const LogInError = styled(P5)`
-  margin-bottom: 8px;
-  color: ${({ theme }) => theme.colors.scarlett100};
-`;
+const LogInPage = () => {
+  const [callback, setCallback] = useState<OAuth2CallbackUrl|null>(null);
+  const [authUrl, setAuthUrl] = useState<string|null>(null);
+  const [isAuthing, setIsAuthing] = useState(false);
 
-const LogInPage: FC = () => {
+  const { context } = useDeskproLatestAppContext();
   const { client } = useDeskproAppClient();
-  const [state, dispatch] = useStore();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-  const [authUrl, setAuthUrl] = useState("");
-  const [callback, setCallback] = useState<OAuth2StaticCallbackUrl | null>(
-    null
-  );
 
-  if (error) {
-    console.error(`Trello: ${error}`);
-  }
-
-  useEffect(() => {
-    if (state?.context?.settings.api_key && callback?.callbackUrl) {
-      setAuthUrl(
-        `https://trello.com/1/authorize?${getQueryParams({
-          expiration: "never",
-          scope: "read,write",
-          response_type: "token",
-          key: state?.context?.settings.api_key,
-          return_url: callback?.callbackUrl,
-        })}`
+  useInitialisedDeskproAppClient((client) => {
+    (async () => {
+      const callback: OAuth2CallbackUrl = await client.oauth2().getCallbackUrl(
+        'auth',
+        /#token=(?<token>.*)&?/,
+        { pollInterval: 2000 }
       );
 
-      setLoading(false);
-    }
-  }, [state?.context?.settings.api_key, callback?.callbackUrl]);
+      setCallback(() => callback);
+    })();
+  }, [setCallback]);
 
   useEffect(() => {
-    if (!client) {
-      return;
+    if (callback?.callbackUrl && context?.settings.client_key) {
+      setAuthUrl(`https://trello.com/1/authorize?expiration=never&name=deskpro&scope=read&key=${context?.settings.client_key}&callback_method=fragment&return_url=${callback?.callbackUrl}`);
     }
+  }, [callback, context?.settings]);
 
-    client?.deregisterElement("trelloPlusButton");
-    client?.deregisterElement("trelloHomeButton");
-    client?.deregisterElement("trelloExternalCtaLink");
-    client?.deregisterElement("trelloMenu");
-    client?.deregisterElement("trelloEditButton");
-  }, [client]);
+  const poll = () => {
+    (async () => {
+      setIsAuthing(true);
 
-  const onSignIn = () => {
-    if (!callback) {
-      return;
-    }
+      if (callback?.poll) {
+        // let's remove any previous tokens
+        await client?.deleteUserState('oauth2/auth');
 
-    setLoading(true);
+        // poll for the new token
+        const { statePathPlaceholder } = await callback.poll();
 
-    callback
-      ?.poll()
-      .then(() => {
-        return checkIsAliveService(client as IDeskproClient);
-      })
-      .then((data) => {
-        if ((data as { isAlive: boolean }).isAlive) {
-          dispatch({ type: "setAuth", isAuth: true });
-        }
-      })
-      .catch((error) => dispatch({ type: "error", error }))
-      .finally(() => setLoading(false));
+        // todo: captured token, you can now use it for further requests
+
+        console.log("token placeholder", statePathPlaceholder);
+
+        setIsAuthing(false);
+      }
+    })();
   };
 
-  useSetAppTitle("Trello");
-
-  useEffect(() => {
-    if (!client || callback) {
-      return;
-    }
-
-    setLoading(true);
-
-    client
-      .oauth2()
-      .getGenericCallbackUrl("http", /#token=(?<token>.*)&?/, /(?<key>http)/)
-      .then((callback) => {
-        setCallback(callback);
-      })
-      .finally(() => setLoading(false));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [client, state.isAuth]);
-
-  return loading ? (
-    <Loading />
-  ) : (
+  return (
     <>
-      <H3 style={{ marginBottom: !error ? 14 : 2 }}>
+      <H3>
         Log into your Trello Account
       </H3>
-      {error && <LogInError>An error occurred, please try again.</LogInError>}
-      <AnchorButton
-        text="Sign In"
-        href={authUrl}
-        target="_blank"
-        onClick={onSignIn}
-      />
+      {authUrl && (
+        <AnchorButton
+          text="Sign In"
+          href={authUrl}
+          target="_blank"
+          loading={isAuthing}
+          onClick={poll}
+        />
+      )}
     </>
   );
 };
