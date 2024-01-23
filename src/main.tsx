7516ed830614ae3a6fa6d08fbcaa2858214fccfe
ilia makarov <ilia.makarov@me.com>
2024-01-23 18:44:01 +0200

    Just for testing

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,6 @@ import "iframe-resizer/js/iframeResizer.contentWindow.js";
 import "flatpickr/dist/themes/light.css";
 import "tippy.js/dist/tippy.css";
 import "simplebar/dist/simplebar.min.css";
-
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 
