f4a82f7796a34a2de80982db9d985586fd203d3c
ilia makarov <ilia.makarov@me.com>
2024-06-04 18:03:35 +0300

    Removed iframe-resizer

--- src/main.tsx
+++ src/main.tsx
@@ -10,11 +10,9 @@ import { queryClient } from "./query";
 import { ReplyBoxProvider } from "./hooks";
 import { App } from "./App";
 import { ErrorFallback } from "./components";
-import "iframe-resizer/js/iframeResizer.contentWindow.js";
 import "flatpickr/dist/themes/light.css";
 import "tippy.js/dist/tippy.css";
 import "simplebar/dist/simplebar.min.css";
-
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 
