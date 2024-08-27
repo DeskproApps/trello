4d50d83eaabf3942173d878a1eba56042524ea64
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2024-08-27 13:24:01 +0100

    [Feature] SC-124017 Refactor apps to remove iframe-resizer dependency and make iframe content sizing more robust

--- src/App.tsx
+++ src/App.tsx
@@ -53,24 +53,23 @@ const App = () => {
 
     if (isLoading) {
         return (
-            <LoadingSpinner/>
+            <LoadingSpinner />
         );
     }
 
     return (
         <>
             <Routes>
-                <Route path="/home" element={<HomePage />}/>)
-                <Route path="/log_in" element={<LogInPage />}/>)
-                <Route path="/link_card" element={<LinkCardPage />}/>)
-                <Route path="/create_card" element={<CreateCardPage/>}/>)
-                <Route path="/view_card/:cardId" element={<ViewCardPage />}/>)
-                <Route path="/edit_card/:cardId" element={<EditCardPage />}/>)
-                <Route path="/add_comment/:cardId" element={<AddCommentPage />}/>)
-                <Route path="/admin/callback" element={<AdminCallbackPage/>}/>)
-                <Route index element={<LoadingAppPage/>} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/log_in" element={<LogInPage />} />
+                <Route path="/link_card" element={<LinkCardPage />} />
+                <Route path="/create_card" element={<CreateCardPage />} />
+                <Route path="/view_card/:cardId" element={<ViewCardPage />} />
+                <Route path="/edit_card/:cardId" element={<EditCardPage />} />
+                <Route path="/add_comment/:cardId" element={<AddCommentPage />} />
+                <Route path="/admin/callback" element={<AdminCallbackPage />} />
+                <Route index element={<LoadingAppPage />} />
             </Routes>
-            {!isAdmin && (<><br/><br/><br/></>)}
         </>
     );
 };
