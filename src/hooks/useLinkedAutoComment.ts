e1c85f7150b93b1fe5a26d5d807cb297972f9deb
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-01-14 17:17:06 +0000

    annotate ticketID and permalink as strings

--- src/hooks/useLinkedAutoComment.ts
+++ src/hooks/useLinkedAutoComment.ts
@@ -28,8 +28,8 @@ const useLinkedAutoComment = (): Result => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const isEnable = get(context, ["settings", "add_comment_when_linking"], false);
-    const ticketId = get(context, ["data", "ticket", "id"]);
-    const permalink = get(context, ["data", "ticket", "permalinkUrl"]);
+    const ticketId: string = get(context, ["data", "ticket", "id"]);
+    const permalink: string = get(context, ["data", "ticket", "permalinkUrl"]);
 
     const addLinkComment = useCallback((cardId: CardType["id"]) => {
         if (!client || !isEnable) {
@@ -37,7 +37,7 @@ const useLinkedAutoComment = (): Result => {
         }
 
         setIsLoading(true);
-        return createCardCommentService(client, cardId, getLinkedMessage(ticketId as string, permalink as string))
+        return createCardCommentService(client, cardId, getLinkedMessage(ticketId, permalink))
             .finally(() => setIsLoading(false));
     }, [client, isEnable, ticketId, permalink]);
 
