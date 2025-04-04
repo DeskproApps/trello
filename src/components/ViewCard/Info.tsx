64cb62d97c0b52bed674fbd032a7575cb3dcdb9d
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2025-04-04 13:31:39 +0100

    trello Edit page not displaying value for [sc-181002]

--- src/components/ViewCard/Info.tsx
+++ src/components/ViewCard/Info.tsx
@@ -1,4 +1,3 @@
-import get from "lodash/get";
 import find from "lodash/find";
 import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
 import { Title, useDeskproAppTheme } from "@deskpro/app-sdk";
@@ -6,101 +5,101 @@ import { P5, Pill, Icon, Stack } from "@deskpro/deskpro-ui";
 import { getDate } from "../../utils/date";
 import { getLabelColor } from "../../utils";
 import {
-    LinkIcon,
-    Markdown,
-    Container,
-    TrelloLogo,
-    EmptyInlineBlock,
-    TextBlockWithLabel,
+  LinkIcon,
+  Markdown,
+  Container,
+  TrelloLogo,
+  EmptyInlineBlock,
+  TextBlockWithLabel,
 } from "../common";
 import { Members } from "../common/Cards";
 import type { FC } from "react";
 import type { CardType, Organization } from "../../services/trello/types";
 
 type Props = {
-    card: CardType,
-    organizations: Organization[],
+  card: CardType,
+  organizations: Organization[],
 };
 
 const Info: FC<Props> = ({ card, organizations }) => {
-    const { theme } = useDeskproAppTheme();
-    const labels = get(card, ["labels"]);
-    const due = get(card, ["due"]);
-    const workspace = find(organizations, {
-        id: get(card, ["board", "idOrganization"]),
-    });
+  const { theme } = useDeskproAppTheme();
+  const labels = card?.labels ?? [];
+  const due = card?.due;
+  const workspace = find(organizations, {
+    id: card?.board?.idOrganization,
+  });
 
-    return (
-        <Container>
-            <Title
-                title={get(card, ["name"], "-")}
-                icon={<TrelloLogo/>}
-                link={get(card, ["shortUrl"], "#")}
-            />
-            <TextBlockWithLabel
-                label="Workspace"
-                text={(
-                    <>
-                        <P5 style={{ marginRight: 4 }}>{get(workspace, ["displayName"], "-")}</P5>
-                        {get(workspace, ["url"]) && (
-                            <LinkIcon size={10} href={get(workspace, ["url"], "#")}/>
-                        )}
-                    </>
-                )}
-            />
-            <TextBlockWithLabel
-                label="Board"
-                text={(
-                    <>
-                        <P5 style={{ marginRight: 4 }}>{get(card, ["board", "name"], "-")}</P5>
-                        <LinkIcon size={10} href={get(card, ["board", "url"], "-")}/>
-                    </>
-                )}
-            />
-            <TextBlockWithLabel
-                label="List"
-                text={get(card, ["list", "name"], "-")}
-            />
-            <TextBlockWithLabel
-                label="Description"
-                text={(
-                    <Markdown text={get(card, ["desc"], "-")} />
-                )}
-            />
-            <TextBlockWithLabel
-                label="Labels"
-                text={(!labels)
-                    ? "-"
-                    : (
-                        <Stack wrap="wrap" gap={6}>
-                            {
-                                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                                labels.map(({ id, name, color }: any) => (
-                                    <Pill
-                                        key={id}
-                                        label={name ? name : <EmptyInlineBlock/>}
-                                        {...getLabelColor(theme, color)}
-                                    />
-                                ))
-                            }
-                        </Stack>
-                    )
-                }
-            />
-            <TextBlockWithLabel
-                label="Due date"
-                text={(
-                    <P5>
-                        {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-                        {/* @ts-ignore */}
-                        <Icon icon={faCalendarDays} style={{ color: theme.colors.grey40 }}/>&nbsp;
-                        {!due ? "-" : getDate(due)}
-                    </P5>
-                )}
-            />
-            <Members members={get(card, ["members"])} />
-        </Container>
-    );
+  return (
+    <Container>
+      <Title
+        title={card?.name ?? "-"}
+        icon={<TrelloLogo />}
+        link={card?.shortUrl ?? "#"}
+      />
+      <TextBlockWithLabel
+        label="Workspace"
+        text={(
+          <>
+            <P5 style={{ marginRight: 4 }}>{workspace?.displayName ?? "-"}</P5>
+            {workspace?.url && (
+              <LinkIcon size={10} href={workspace?.url ?? "#"} />
+            )}
+          </>
+        )}
+      />
+      <TextBlockWithLabel
+        label="Board"
+        text={(
+          <>
+            <P5 style={{ marginRight: 4 }}>{card?.board?.name ?? "-"}</P5>
+            <LinkIcon size={10} href={card?.board?.url ?? "-"} />
+          </>
+        )}
+      />
+      <TextBlockWithLabel
+        label="List"
+        text={card?.list?.name ?? "-"}
+      />
+      <TextBlockWithLabel
+        label="Description"
+        text={(
+          <Markdown text={card?.desc ?? "-"} />
+        )}
+      />
+      <TextBlockWithLabel
+        label="Labels"
+        text={(!labels)
+          ? "-"
+          : (
+            <Stack wrap="wrap" gap={6}>
+              {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                labels.map(({ id, name, color }: any) => (
+                  <Pill
+                    key={id}
+                    label={name ? name : <EmptyInlineBlock />}
+                    {...getLabelColor(theme, color)}
+                  />
+                ))
+              }
+            </Stack>
+          )
+        }
+      />
+      <TextBlockWithLabel
+        label="Due date"
+        text={(
+          <P5>
+            {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+            {/* @ts-ignore */}
+            <Icon icon={faCalendarDays} style={{ color: theme.colors.grey40 }} />&nbsp;
+            {!due ? "-" : getDate(due)}
+          </P5>
+        )}
+      />
+      <Members members={card?.members ?? []} />
+    </Container>
+  );
 };
 
 export { Info };
--- src/components/ViewCard/ViewCard.tsx
+++ src/components/ViewCard/ViewCard.tsx
@@ -1,6 +1,3 @@
-import { useMemo } from "react";
-import get from "lodash/get";
-import size from "lodash/size";
 import { HorizontalDivider } from "@deskpro/app-sdk";
 import { NoFound } from "../common";
 import { Info } from "./Info";
@@ -10,42 +7,40 @@ import type { FC } from "react";
 import type { Maybe } from "../../types";
 import type { CardType, ChecklistItem, Comment, Organization } from "../../services/trello/types";
 
-type Props =  {
-    card: Maybe<CardType>,
-    comments: Maybe<Comment[]>,
-    organizations: Organization[],
-    onNavigateToAddNewComment: () => void,
-    onChangeChecklistItem: (
-        itemId: ChecklistItem["id"],
-        state: ChecklistItem["state"],
-    ) => void,
+type Props = {
+  card: Maybe<CardType>,
+  comments: Maybe<Comment[]>,
+  organizations: Organization[],
+  onNavigateToAddNewComment: () => void,
+  onChangeChecklistItem: (
+    itemId: ChecklistItem["id"],
+    state: ChecklistItem["state"],
+  ) => void,
 };
 
 const ViewCard: FC<Props> = ({
-    card,
-    comments,
-    organizations,
-    onNavigateToAddNewComment,
-    onChangeChecklistItem,
+  card,
+  comments,
+  organizations,
+  onNavigateToAddNewComment,
+  onChangeChecklistItem,
 }) => {
-    const checklists = get(card, ["checklists"]);
-    const isChecklists = useMemo(() => {
-        return Array.isArray(checklists) && Boolean(size(checklists));
-    }, [checklists]);
+  const checklists = card?.checklists ?? [];
+  const isChecklists = checklists.length > 0;
 
-    if (!card) {
-        return (<NoFound/>);
-    }
+  if (!card) {
+    return (<NoFound />);
+  }
 
-    return (
-        <>
-            <Info card={card} organizations={organizations} />
-            {isChecklists && (<HorizontalDivider style={{ marginBottom: 10 }} />)}
-            <CheckLists checklists={checklists} onChangeChecklistItem={onChangeChecklistItem} />
-            <HorizontalDivider style={{ marginBottom: 10 }} />
-            <Comments comments={comments} onClickTitleAction={onNavigateToAddNewComment} />
-        </>
-    );
+  return (
+    <>
+      <Info card={card} organizations={organizations} />
+      {isChecklists && (<HorizontalDivider style={{ marginBottom: 10 }} />)}
+      <CheckLists checklists={checklists} onChangeChecklistItem={onChangeChecklistItem} />
+      <HorizontalDivider style={{ marginBottom: 10 }} />
+      <Comments comments={comments ?? []} onClickTitleAction={onNavigateToAddNewComment} />
+    </>
+  );
 };
 
 export { ViewCard };
